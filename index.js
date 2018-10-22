37653f7034e127b4f8edef3d2c51bc7ee9c73f11
Elizaveta <anthemion@yandex.ru>
2018-10-22 18:50:50 +0200

    Add one more park
    Add one more park

--- index.js
+++ index.js
@@ -21,14 +21,20 @@ const multiplyResult = multiply(additionResult,6)
 
   const bautzen = new Park('Dinosaur Park Bautzen', 'Saurierpark 1, 02625 Bautzen', '9.00 - 18.00')
   console.log(bautzen)
+  var dino = new Park('Park Dino Münchehagen', 'Alte Zollstraße 5, 31547 Rehburg-Loccum', 'daily 9.00 - 18.00')
+  console.log(dino)
 
   armagan.attend(bautzen)
   mert.attend(bautzen)
+  mert.attend(dino)
   bautzen.report()
+  dino.report()
 
   console.log(bautzen.attendees)
+  console.log(dino.attendees)
   console.log(Chalk.red(armagan.name))
 
   Database.save(bautzen)
+  Database.save(dino)
   const loadedFile = Database.load()
   console.log(loadedFile.attendees)
